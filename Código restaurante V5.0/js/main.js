0fe71761a87a52f7d0ba792128e02422e3e450d0
ARIAS OJEDA ESTEBAN RENE <75871827+EstebanAriasO@users.noreply.github.com>
2023-08-06 20:37:46 -0500

    Version 5

---
+++ Código restaurante V5.0/js/main.js
@@ -0,0 +1,8 @@
+function show_popup_login(){
+	let popup = document.querySelector(".popup-login");
+	if(popup.classList.contains('active')){
+		popup.classList.remove('active');
+	}else{
+		popup.classList.add('active');
+	}
+}
